8af4d60b8ee06318ead7efdd98ae752a1c283a59
Toshimitsu Ota <toshimitsu.ota@griffithuni.edu.au>
2023-04-20 16:34:22 +1000

    Add reordering place items feature for onMove

--- FavouritePlaces/View/ContentView.swift
+++ FavouritePlaces/View/ContentView.swift
@@ -14,9 +14,13 @@ struct ContentView: View {
     /// Place name as state variable
     @State var placeName: String = ""
     /// Fetch request to get entity in Place
-    @FetchRequest(entity: Place.entity(), sortDescriptors: [])
+    @FetchRequest(entity: Place.entity(), sortDescriptors: [NSSortDescriptor(key: "position", ascending: true)])
     /// Private variable for result from fetching
     private var places: FetchedResults<Place>
+    ///
+    //private var tempPlaces: [Place] = []
+    /// Environment property for edit mode
+    @Environment(\.editMode) private var editMode
     
     var body: some View {
         NavigationView{
@@ -44,17 +48,33 @@ struct ContentView: View {
                     .onDelete {
                         index in
                         deletePlace(index)
+                        saveContext()
+                    }
+                    .onMove{
+                        index, position in
+                        movePlace(index, position)
+                        saveContext()
                     }
                 }
+                
             }
             .navigationTitle("My Favourite Places")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                }
+            }
         }
     }
     
     private func addPlace() {
         withAnimation{
+            guard placeName != "" else {
+                return
+            }
             let place = Place(context: viewContext)
             place.name = placeName
+            place.position = sortPlaces()
             saveContext()
         }
     }
@@ -64,8 +84,31 @@ struct ContentView: View {
             index.map{places[$0]}.forEach{
                 place in viewContext.delete(place)
             }
-            saveContext()
         }
+    }
+    
+    /// Update position attribute of Place items to the new order of items in List. Changes made to reference type copied places array will update the core data automatically. (Source: https://stackoverflow.com/questions/59742218/swiftui-reorder-coredata-objects-in-list)
+    ///
+    /// - parameter index:
+    private func movePlace(_ index: IndexSet, _ position: Int) {
+        /// Make a copy array of items
+        var movingItems = places.map{$0}
+        /// Change orders of items to new potitions
+        movingItems.move(fromOffsets: index, toOffset: position)
+        /// Update position attributes in reversed order to minimise changes
+        for reverseIndex in stride(from: movingItems.count - 1, through: 0, by: -1) {
+            movingItems[reverseIndex].position = Int16(reverseIndex)
+        }
+    }
+    
+    private func sortPlaces() -> Int16 {
+        var position: Int16 = 0
+        places.forEach{
+            place in
+            place.position = position
+            position += 1
+        }
+        return position
     }
 
     private func saveContext() {
