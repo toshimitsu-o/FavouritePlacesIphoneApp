a0febe9e305b81ce450664c2dd972d94cad7ec14
Toshimitsu Ota <toshimitsu.ota@griffithuni.edu.au>
2023-04-20 12:07:28 +1000

    Initial Commit

---
+++ FavouritePlaces/FavouritePlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouritePlacesApp.swift
+//  FavouritePlaces
+//
+//  Created by Toshimitsu Ota on 20/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct FavouritePlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
