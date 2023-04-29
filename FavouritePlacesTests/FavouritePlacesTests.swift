0d2b1e9ffb69592b297651e6cbab033bc251b78d
Toshimitsu Ota <toshimitsu.ota@griffithuni.edu.au>
2023-04-29 19:57:09 +1000

    Add unit test functions

--- FavouritePlacesTests/FavouritePlacesTests.swift
+++ FavouritePlacesTests/FavouritePlacesTests.swift
@@ -6,9 +6,12 @@
 //
 
 import XCTest
+import CoreData
 @testable import FavouritePlaces
 
 final class FavouritePlacesTests: XCTestCase {
+    
+    let context = PersistenceController.shared.container.viewContext
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -18,19 +21,46 @@ final class FavouritePlacesTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testNameString() throws {
+        let place = Place(context: context)
+        let name = place.nameString
+        XCTAssertEqual(name, "No name")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testNotesString() throws {
+        let place = Place(context: context)
+        let notes = place.notesString
+        XCTAssertEqual(notes, "No notes")
+    }
+    
+    func testLatitudeString() throws {
+        let place = Place(context: context)
+        place.latitude = 23.02
+        let latitudeString = place.latitudeString
+        XCTAssertEqual(latitudeString, "23.02")
+    }
+    
+    func testLongitudeString() throws {
+        let place = Place(context: context)
+        place.longitude = 23.02
+        let longitudeString = place.longitudeString
+        XCTAssertEqual(longitudeString, "23.02")
+    }
+    
+    func testUrlString() throws {
+        let place = Place(context: context)
+        let url = "http://www.no.no/img/no.png"
+        place.urlString = url
+        let urlString = place.urlString
+        XCTAssertEqual(urlString, url)
+    }
+    
+    func testGetImage() async throws {
+        let place = Place(context: context)
+        let url = "http://www.no.no/img/no.png"
+        place.urlString = url
+        let image = await place.getImage()
+        XCTAssertEqual(image, defaultImage)
     }
 
 }
