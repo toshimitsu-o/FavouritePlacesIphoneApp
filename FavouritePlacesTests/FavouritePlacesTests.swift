4faa1688a5fbd79c09e31abe25f943c221c0a414
Toshimitsu Ota <toshimitsu.ota@griffithuni.edu.au>
2023-04-29 20:30:46 +1000

    Add documentation and comments

--- FavouritePlacesTests/FavouritePlacesTests.swift
+++ FavouritePlacesTests/FavouritePlacesTests.swift
@@ -11,54 +11,67 @@ import CoreData
 
 final class FavouritePlacesTests: XCTestCase {
     
+    /// Set context to use in unit test functions
     let context = PersistenceController.shared.container.viewContext
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
+    
+    /// Test nameString from Place to check if it set default value
     func testNameString() throws {
+        /// New Place item
         let place = Place(context: context)
+        /// Property to store value from nameString
         let name = place.nameString
         XCTAssertEqual(name, "No name")
     }
     
+    /// Test notesString from Place to check if it set default value
     func testNotesString() throws {
+        /// New Place item
         let place = Place(context: context)
+        /// property to store value from notesString
         let notes = place.notesString
         XCTAssertEqual(notes, "No notes")
     }
     
+    /// Test latitudeString from Place to check if it converts Double to srting
     func testLatitudeString() throws {
+        /// New Place item
         let place = Place(context: context)
         place.latitude = 23.02
+        /// Property to store value from latitudeString
         let latitudeString = place.latitudeString
         XCTAssertEqual(latitudeString, "23.02")
     }
     
+    /// Test longitudeString from Place to check if it converts Double to srting
     func testLongitudeString() throws {
+        /// New Place item
         let place = Place(context: context)
         place.longitude = 23.02
+        /// Property to store value from longitudeString
         let longitudeString = place.longitudeString
         XCTAssertEqual(longitudeString, "23.02")
     }
     
+    /// Test urlString from Place to check if it handle URL correctly
     func testUrlString() throws {
+        /// New Place item
         let place = Place(context: context)
+        /// Fake URL oft image
         let url = "http://www.no.no/img/no.png"
         place.urlString = url
+        /// Property to store value from urlString
         let urlString = place.urlString
         XCTAssertEqual(urlString, url)
     }
     
+    /// Test getImage method from Place to check if it return default image when image can't be downloaded
     func testGetImage() async throws {
+        /// New Place item
         let place = Place(context: context)
+        /// Fake URL oft image
         let url = "http://www.no.no/img/no.png"
         place.urlString = url
+        /// Property to store result from getImage method
         let image = await place.getImage()
         XCTAssertEqual(image, defaultImage)
     }
